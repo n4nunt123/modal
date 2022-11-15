bc106c98efcbca1c04c0e6288a9de46dd78b27a6
unknown <ihellostereo@gmail.com>
2022-11-15 11:29:19 +0700

    maintenance controller

--- routes/maintenance.js
+++ routes/maintenance.js
@@ -2,5 +2,10 @@ const express = require('express')
 const maintenance = express.Router()
 const MaintenanceController = require('../controller/maintenanceController')
 
+maintenance.get('/maintenances', MaintenanceController.getMaintenances)
+maintenance.get('/maintenances/:id', MaintenanceController.getMaintenance)
+maintenance.post('/maintenances', MaintenanceController.insertMaintenance)
+maintenance.patch('/maintenances/:id', MaintenanceController.updateMaintenance)
+maintenance.delete('/maintenances/:id', MaintenanceController.deleteMaintenance)
 
 module.exports = maintenance
